d831e4a1722bf14c4cd1bcdd41d1129f6d53bebf
Roman <lugovijroman39@gmail.com>
2025-05-31 00:18:07 +0300

    setted up project boilerplate

---
+++ PDFConvertor/DTOs/ConvertationErrors/ConvertationResult.cs
@@ -0,0 +1,30 @@
+﻿using PdfSharpCore.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFConvertor.DTOs.ConvertationErrors
+{
+    internal class ConvertationResult
+    {
+        public bool IsSuccess { get; init; }
+        public ConvertationErrorCode? Error { get; init; }
+        public PdfDocument? Document { get; init; }
+
+        public static ConvertationResult Success(PdfDocument document) => new()
+        {
+            IsSuccess = true,
+            Document = document,
+        };
+
+        public static ConvertationResult Fail(ConvertationErrorCode error) => new()
+        {
+            IsSuccess = false,
+            Error = error
+        };
+
+        public static ConvertationResult Success() => new() { IsSuccess = true };
+    }
+}
---
+++ PDFConvertor/DTOs/FileInputDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFConvertor.DTOs
+{
+    internal class FileInputDTO
+    {
+        public string FilePath { get; set; } = string.Empty;
+        public string OutputPath {  get; set; } = string.Empty;
+    }
+}
